b1187594dc693a20e8cc1a0573e9aecca4ab8c31
Yosuke Yoh <yosukeonrails@gmail.com>
2017-03-11 13:01:23 -0800

    created function to group everything into rows of 3

--- js/components/saladdata.js
+++ js/components/saladdata.js
@@ -1,187 +1,203 @@
 
-{
-	  "eng":"cherry Tomato",
-      "cant":"cherry Tomato",
-      "mand":"cherry Tomato",
-       "stnUnit":"lb",
-     "stnCurrency":"USD",
-     "pricePerUnit":2,
-     "pictureUrl":"String",
-    "hkd_priceperunit":0,
-    "usd_priceperunit":0,
-    "cny_priceperunit":0,
-	 "id":"4442521"
-}
-
 var veggieData= [
-{
- "name":{
-       "eng":"Salad Pack",
-	"cant": "" ,
-	 "mand": ""  ,
+	{
+ name:{
+	eng:"Salad Pack",
+	cant: "" ,
+	 mand: ""  ,
  },
- "stnUnit":"pack",
-  "stnCurrency":"USD" ,
-  "pricePerUnit":5,
-   "pictureUrl":"",
-  "PricePerStnUnit":{
-      "usd_priceperunit":0,
-      "HKD":null,
-      "CNY":null
-  }
+ stnUnit:"pack",
+  stnCurrency:"USD" ,
+  pricePerUnit:5,
+   pictureUrl:"",
+  PricePerStnUnit:{
+      usd_priceperunit:0,
+      hkd_priceperunit:0,
+      cny_priceperunit:0
+  },
+	id:'1314'
 },
 
 
 {
- "name":{
-  "eng":"Cherry Tomato",
-	"cant":"",
-	 "mand":"",
+ name:{
+  eng:"Cherry Tomato",
+	cant:"",
+	 mand:"",
 		},
- "stnUnit":"lb",
-  "stnCurrency":"USD",
-  "pricePerUnit":1.5,
-   "pictureUrl":"http://s1149.photobucket.com/user/Yosuke_Ayrton_Hishinuma/media/cherry-tomatoes_zpstanw2dai.jpg.html?o=20",
-  "PricePerStnUnit":{
-      "hkd_priceperunit":0,
-      "usd_priceperunit":0,
-      "cny_priceperunit":0
-  }
+ stnUnit:"lb",
+  stnCurrency:"USD",
+  pricePerUnit:1.5,
+   pictureUrl:"http://s1149.photobucket.com/user/Yosuke_Ayrton_Hishinuma/media/cherry-tomatoes_zpstanw2dai.jpg.html?o=20",
+  PricePerStnUnit:{
+		usd_priceperunit:0,
+		hkd_priceperunit:0,
+		cny_priceperunit:0
+  },
+	id:'1312'
+},
+{
+ name:{
+       eng:"Meatballs",
+	cant:"",
+	 mand:"",
+		},
+ stnUnit:"lb",
+  stnCurrency:"USD",
+  pricePerUnit:5.2,
+   pictureUrl:"https://i.ytimg.com/vi/zzEaehrIXD4/maxresdefault.jpg" ,
+  PricePerStnUnit:{
+      usd_priceperunit:0,
+      hkd_priceperunit:0,
+      cny_priceperunit:0
+  },
+	id:'133354'
 },
 
 {
- "name":{
-       "eng":"",
-	"cant":"",
-	 "mand":"",
+ name:{
+       eng:"Soup Pack",
+	cant:"",
+	 mand:"",
 		},
- "stnUnit":"",
-  "stnCurrency":"",
-  "pricePerUnit":  ,
-   "pictureUrl":"" ,
-  "PricePerStnUnit":{
-      "USD":null,
-      "HKD":null,
-      "CNY":null
-  }
-},
-{
- "name":{
-       "eng":"Meatballs",
-	"cant":"",
-	 "mand":"",
-		},
- "stnUnit":"lb",
-  "stnCurrency":"USD",
-  "pricePerUnit":5.2,
-   "pictureUrl":"" ,
-  "PricePerStnUnit":{
-      "USD":null,
-      "HKD":null,
-      "CNY":null
-  }
+ stnUnit:"pack",
+  stnCurrency:"USD",
+  pricePerUnit:3,
+   pictureUrl:"http://img.finedininglovers.com/?img=http%3A%2F%2Ffinedininglovers.cdn.crosscast-system.com%2FBlogPost%2FOriginal_1799_wonton-soup-00244272-CUT1.jpg&w=1200&h=660&lu=1349715714&ext=.jpg" ,
+  PricePerStnUnit:{
+      usd_priceperunit:0,
+      hkd_priceperunit:0,
+      cny_priceperunit:0
+  },
+	id:'132414'
 },
 
 {
- "name":{
-       "eng":"Soup Pack",
-	"cant":"",
-	 "mand":"",
+ name:{
+
+       eng:"Flowering Cabbage (Choi Sum)",
+	cant:"",
+	 mand:"",
 		},
- "stnUnit":"pack",
-  "stnCurrency":"USD",
-  "pricePerUnit":3,
-   "pictureUrl":"" ,
-  "PricePerStnUnit":{
-      "USD":null,
-      "HKD":null,
-      "CNY":null
-  }
+ stnUnit:"catty",
+  stnCurrency:"USD",
+  pricePerUnit:1.3,
+   pictureUrl:"http://lakeviewfarmsnc.com/wp-content/uploads/2014/08/flowering-cabbage-3.jpg" ,
+  PricePerStnUnit:{
+      usd_priceperunit:0,
+      hkd_priceperunit:0,
+      cny_priceperunit:0
+  },
+	id:'131654364'
 },
 
 {
- "name":{
-
-       "eng":"Flowering Cabbage (Choi Sum)",
-	"cant":"",
-	 "mand":"",
+ name:{
+       eng:"Chinese Kale",
+	cant:"",
+	 mand:"",
 		},
- "stnUnit":"catty",
-  "stnCurrency":"USD",
-  "pricePerUnit":1.3,
-   "pictureUrl":"" ,
-  "PricePerStnUnit":{
-      "USD":null,
-      "HKD":null,
-      "CNY":null
-  }
+ stnUnit:"catty",
+  stnCurrency:"USD",
+  pricePerUnit:1.3,
+   pictureUrl:"http://image.made-in-china.com/43f34j00dywtnaWsQvoi/Chinese-Dried-Flake-Ginger-Peeled.jpg" ,
+  PricePerStnUnit:{
+      usd_priceperunit:0,
+      hkd_priceperunit:0,
+      cny_priceperunit:0
+  },
+	id:'1434314'
 },
 
 {
- "name":{
-       "eng":"Chinese Kale",
-	"cant":"",
-	 "mand":"",
+ name:{
+       eng:"Tomato",
+	cant:"",
+	 mand:"",
 		},
- "stnUnit":"catty",
-  "stnCurrency":"USD",
-  "pricePerUnit":1.3,
-   "pictureUrl":"" ,
-  "PricePerStnUnit":{
-      "USD":null,
-      "HKD":null,
-      "CNY":null
-  }
+ stnUnit:"catty",
+  stnCurrency:"USD",
+  pricePerUnit:1.3,
+   pictureUrl:"http://cdn2.stylecraze.com/wp-content/uploads/2013/05/tomato-hair-benefits1.jpg" ,
+  PricePerStnUnit:{
+      usd_priceperunit:0,
+      hkd_priceperunit:0,
+      cny_priceperunit:0
+  },
+	id:'1434314'
+},
+{
+ name:{
+       eng:"Okra",
+	cant:"",
+	 mand:"",
+		},
+ stnUnit:"pack",
+  stnCurrency:"USD",
+  pricePerUnit:2,
+   pictureUrl:"http://toriavey.com/images/2013/08/The-Secret-History-of-Okra-3.jpg" ,
+  PricePerStnUnit:{
+      usd_priceperunit:0,
+      hkd_priceperunit:0,
+      cny_priceperunit:0
+  },
+	id:'1343514'
 },
 
 {
- "name":{
-       "eng":"Tomato",
-	"cant":"",
-	 "mand":"",
+ name:{
+       eng:"Purple Sweet Potato",
+	cant:"",
+	 mand:"",
 		},
- "stnUnit":"catty",
-  "stnCurrency":"USD",
-  "pricePerUnit":1.3,
-   "pictureUrl":"" ,
-  "PricePerStnUnit":{
-      "USD":null,
-      "HKD":null,
-      "CNY":null
-  }
-},
-{
- "name":{
-       "eng":"Okra",
-	"cant":"",
-	 "mand":"",
-		},
- "stnUnit":"pack",
-  "stnCurrency":"USD",
-  "pricePerUnit":2,
-   "pictureUrl":"" ,
-  "PricePerStnUnit":{
-      "USD":null,
-      "HKD":null,
-      "CNY":null
-  }
+ stnUnit:"catty",
+  stnCurrency:"USD",
+  pricePerUnit:0.80  ,
+   pictureUrl:"https://www.google.com/imgres?imgurl=https://twistedcheft.files.wordpress.com/2013/07/purple-yam.jpg&imgrefurl=https://twistedcheft.com/2013/07/19/purple-yam-soup/&h=3497&w=3447&tbnid=W_538OAaGWvUfM:&vet=1&tbnh=186&tbnw=183&docid=-HqjP6n8R1y05M&itg=1&usg=__0rtmSmciENjnKFObZqRgwMM_r50=&sa=X&ved=0ahUKEwjNz_eWqs_SAhUJ52MKHdNgBScQ_B0IejAK&ei=NmPEWM3cHInOjwPTwZW4Ag#h=3497&imgrc=W_538OAaGWvUfM:&tbnh=186&tbnw=183&vet=1&w=3447" ,
+  PricePerStnUnit:{
+      usd_priceperunit:0,
+      hkd_priceperunit:0,
+      cny_priceperunit:0
+  },
+	id:'131535'
 },
 
 {
- "name":{
-       "eng":"Purple Sweet Potato",
-	"cant":"",
-	 "mand":"",
+ name:{
+       eng:"Purple Sweet Potato",
+	cant:"",
+	 mand:"",
 		},
- "stnUnit":"catty",
-  "stnCurrency":"USD",
-  "pricePerUnit":0.80  ,
-   "pictureUrl":"" ,
-  "PricePerStnUnit":{
-      "USD":null,
-      "HKD":null,
-      "CNY":null
-  }
+ stnUnit:"catty",
+  stnCurrency:"USD",
+  pricePerUnit:0.80  ,
+   pictureUrl:"https://www.google.com/imgres?imgurl=https://twistedcheft.files.wordpress.com/2013/07/purple-yam.jpg&imgrefurl=https://twistedcheft.com/2013/07/19/purple-yam-soup/&h=3497&w=3447&tbnid=W_538OAaGWvUfM:&vet=1&tbnh=186&tbnw=183&docid=-HqjP6n8R1y05M&itg=1&usg=__0rtmSmciENjnKFObZqRgwMM_r50=&sa=X&ved=0ahUKEwjNz_eWqs_SAhUJ52MKHdNgBScQ_B0IejAK&ei=NmPEWM3cHInOjwPTwZW4Ag#h=3497&imgrc=W_538OAaGWvUfM:&tbnh=186&tbnw=183&vet=1&w=3447" ,
+  PricePerStnUnit:{
+      usd_priceperunit:0,
+      hkd_priceperunit:0,
+      cny_priceperunit:0
+  },
+	id:'131535'
+},
+
+{
+ name:{
+       eng:"Purple Sweet Potato",
+	cant:"",
+	 mand:"",
+		},
+ stnUnit:"catty",
+  stnCurrency:"USD",
+  pricePerUnit:0.80  ,
+   pictureUrl:"https://www.google.com/imgres?imgurl=https://twistedcheft.files.wordpress.com/2013/07/purple-yam.jpg&imgrefurl=https://twistedcheft.com/2013/07/19/purple-yam-soup/&h=3497&w=3447&tbnid=W_538OAaGWvUfM:&vet=1&tbnh=186&tbnw=183&docid=-HqjP6n8R1y05M&itg=1&usg=__0rtmSmciENjnKFObZqRgwMM_r50=&sa=X&ved=0ahUKEwjNz_eWqs_SAhUJ52MKHdNgBScQ_B0IejAK&ei=NmPEWM3cHInOjwPTwZW4Ag#h=3497&imgrc=W_538OAaGWvUfM:&tbnh=186&tbnw=183&vet=1&w=3447" ,
+  PricePerStnUnit:{
+      usd_priceperunit:0,
+      hkd_priceperunit:0,
+      cny_priceperunit:0
+  },
+	id:'131535'
 }
 
 ];
+
+export default veggieData;
--- js/components/shopbrowser.js
+++ js/components/shopbrowser.js
@@ -7,7 +7,7 @@ var router = require('react-router');
 var Router = router.Router;
 var Route = router.Route;
 var Link = router.Link;
-
+import veggieData from './saladdata.js';
 import {connect} from 'react-redux';
 import {push} from 'react-router-redux';
 import {hashHistory} from 'react-router';
@@ -20,6 +20,28 @@ export class ShopBrowser extends React.Component {
      }
 
     render(){
+
+var itemDataArray=veggieData;
+
+var renderedRow=[];
+var groupOfThree=[];
+      var groupRow= function(from , array){
+         				 if(!array[from]){
+                         renderedRow.push(groupOfThree);
+                         return renderedRow;
+                      }
+                 if(groupOfThree.length==3){
+                          renderedRow.push(groupOfThree);
+                          groupOfThree=[];
+                           groupOfThree.push(array[from]);
+                    }else{
+                       groupOfThree.push(array[from]);
+                    }
+                    groupRow(from+1, array);
+             			return renderedRow;
+             } ;
+
+      console.log(groupRow(0, itemDataArray));   
 
      return (
 
